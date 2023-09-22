f093252debcc93d4d98e78c7db7dbfe2ee0ed280
diegwl <diegocastanlopes0210@outlook.com>
2023-09-22 16:40:07 -0300

    refatorando

--- cursos/views.py
+++ cursos/views.py
@@ -1,41 +1,53 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 
+# from .models import Curso, Avaliacao
+# from .serializers import CursoSerializer, AvaliacaoSerializer
+
+# class CursoAPIView(APIView):
+#     """
+#     API de Cursos
+#     """
+#     def get(self, request):
+#         if request.method == 'GET':
+#             cursos = Curso.objects.all()
+#             serializer = CursoSerializer(cursos, many=True)
+#             return Response(serializer.data)
+        
+#     def post(self, request):
+#         if request.method == 'POST':
+#             serializer = CursoSerializer(data=request.data)
+#             serializer.is_valid(raise_exception=True)
+#             serializer.save()
+#             return Response({"msg": "Curso criado com sucesso.", "curso": serializer.data['titulo']}, status=status.HTTP_201_CREATED)
+    
+
+# class AvaliacaoAPIView(APIView):
+#     """
+#     API de Avaliações
+#     """
+#     def get(self, request):
+#         if request.method == 'GET':
+#             avaliacoes = Avaliacao.objects.all()
+#             serializer = AvaliacaoSerializer(avaliacoes, many=True)
+#             return Response(serializer.data)
+        
+#     def post(self, request):
+#         if request.method == 'POST':
+#             serializer = AvaliacaoSerializer(data=request.data)
+#             serializer.is_valid(raise_exception=True)
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+from rest_framework.generics import ListCreateAPIView
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
-    """
-    API de Cursos
-    """
-    def get(self, request):
-        if request.method == 'GET':
-            cursos = Curso.objects.all()
-            serializer = CursoSerializer(cursos, many=True)
-            return Response(serializer.data)
-        
-    def post(self, request):
-        if request.method == 'POST':
-            serializer = CursoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response({"msg": "Curso criado com sucesso.", "curso": serializer.data['titulo']}, status=status.HTTP_201_CREATED)
+class CursoAPIView(ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
     
-
-class AvaliacaoAPIView(APIView):
-    """
-    API de Avaliações
-    """
-    def get(self, request):
-        if request.method == 'GET':
-            avaliacoes = Avaliacao.objects.all()
-            serializer = AvaliacaoSerializer(avaliacoes, many=True)
-            return Response(serializer.data)
-        
-    def post(self, request):
-        if request.method == 'POST':
-            serializer = AvaliacaoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+class AvaliacaoAPIView(ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
