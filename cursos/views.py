d04d3a09319c647f4e3b1cc1843dff4ccaa69a7f
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-10-06 09:29:08 -0300

    update viewsets

--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, mixins
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 from django.shortcuts import get_object_or_404
@@ -6,6 +6,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import status
+
 
 """
 API V1
@@ -50,7 +52,23 @@ class CursoViewSet(viewsets.ModelViewSet):
         curso = self.get_object()
         serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
         return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'], url_path='avaliacoes/(?P<avaliacao_id>[^/.]+)')
+    def get_avaliacao_by_id(self, request, pk=None, avaliacao_id=None):
+        try:
+            avaliacao = Avaliacao.objects.get(pk=avaliacao_id)
+            serializer = AvaliacaoSerializer(avaliacao)
+            return Response(serializer.data)
+        except Avaliacao.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
 
-class AvaliacaoViewSet(viewsets.ModelViewSet):
+    
+# class AvaliacaoViewSet(viewsets.ModelViewSet):
+#     queryset = Avaliacao.objects.all()
+#     serializer_class = AvaliacaoSerializer
+
+# Criar viewset apenas com métodos específicos
+class AvaliacaoViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
