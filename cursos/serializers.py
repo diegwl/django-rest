7ac49afc2bbe5361ecc2256cdd873f6b2ca262f3
Diegwl <diegocastanlopes0210@outlook.com>
2023-10-09 02:36:19 -0300

    campos derivados

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from .models import Curso, Avaliacao
 
@@ -20,15 +21,22 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'ativo',
         )
         
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):
+            return valor
+        raise serializers.ValidationError('A avaliação precisa estar entre 1 e 5.')
+        
 class CursoSerializer(serializers.ModelSerializer):
     # Nested Relationship - Mostra todas as informações dos dados extras
     # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
 
     # HyperLinked Related Field - Cria um link para os dados extras
     avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
-
+    
     # Primary Key Related Field - Traz apenas os ids para os dados extras
     # avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    media_avaliacoes = serializers.SerializerMethodField()
 
     class Meta:
         model = Curso
@@ -38,5 +46,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+        
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        
+        if media is None:
+            return 0
+        return round(media * 2) / 2
