b827395f16c685794b9c38bb727fdf4b6e286fc5
Diegwl <diegocastanlopes0210@outlook.com>
2023-10-09 02:43:12 -0300

    api escola - geek university

---
+++ api - escola/cursos/views.py
@@ -0,0 +1,91 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, mixins
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework import permissions, authentication
+
+from .permissions import EhSuperUser
+
+
+"""
+API V1
+"""
+
+class CursosAPIView(ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
+class CursoAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class AvaliacoesAPIView(ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+    
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+    
+class AvaliacaoAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+    
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+    
+"""
+API V2
+"""
+
+class CursoViewSet(viewsets.ModelViewSet):
+    permission_classes = (
+        EhSuperUser,
+        permissions.DjangoModelPermissions,
+    )
+    authentication_classes = (authentication.TokenAuthentication, )
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        self.pagination_class.page_size = 2
+        avaliacoes = Avaliacao.objects.filter(curso_id=pk)
+        page = self.paginate_queryset(avaliacoes)
+        
+        if page is not None:
+            serializer = AvaliacaoSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'], url_path='avaliacoes/(?P<avaliacao_id>[^/.]+)')
+    def get_avaliacao_by_id(self, request, pk=None, avaliacao_id=None):
+        try:
+            avaliacao = Avaliacao.objects.get(pk=avaliacao_id)
+            serializer = AvaliacaoSerializer(avaliacao)
+            return Response(serializer.data)
+        except Avaliacao.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+    
+# class AvaliacaoViewSet(viewsets.ModelViewSet):
+#     queryset = Avaliacao.objects.all()
+#     serializer_class = AvaliacaoSerializer
+
+# Criar viewset apenas com métodos específicos
+class AvaliacaoViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
